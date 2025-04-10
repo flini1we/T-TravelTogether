249ef70332f01c06b306aad3dd546dc353e4049f
Danil Zabinskij <rexportydf@mail.ru>
2025-04-11 00:22:49 +0300

    setup swiftlint

--- TTravelTogether/ViewController.swift
+++ TTravelTogether/ViewController.swift
@@ -1,19 +1,3 @@
-//
-//  ViewController.swift
-//  TTravelTogether
-//
-//  Created by Данил Забинский on 10.04.2025.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+final class ViewController: UIViewController { }
