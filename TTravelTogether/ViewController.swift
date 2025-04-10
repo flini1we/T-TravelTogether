28e939adc47a90761a581a8e0642e7c93a074999
Danil Zabinskij <rexportydf@mail.ru>
2025-04-10 22:51:02 +0300

    initial commit

---
+++ TTravelTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTravelTogether
+//
+//  Created by Данил Забинский on 10.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
