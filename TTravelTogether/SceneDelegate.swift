5002c23e7741abdf425b80f08cd8569e5bfdd858
Danil Zabinskij <rexportydf@mail.ru>
2025-04-12 22:28:32 +0300

    scene delegate update

--- TTravelTogether/SceneDelegate.swift
+++ TTravelTogether/SceneDelegate.swift
@@ -7,7 +7,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+
+        let mainNavigationContoller = UINavigationController(rootViewController: LoginController())
+        window?.rootViewController = mainNavigationContoller
         window?.makeKeyAndVisible()
     }
 
