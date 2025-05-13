976ac075f56dab032fd464a909ac85dd3e4b01da
Danil Zabinskij <rexportydf@mail.ru>
2025-05-13 20:17:28 +0300

    УРА ЗАФИКСИЛ

---
+++ TTravelTogether/Modules/TripDetailScreen/TripDetailViewModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Combine
+
+final class TripDetailViewModel: ITripDetailViewModel {
+
+    var tripId: UUID
+
+    @Published var tripDetail: TripDetail = .fake()
+
+    var tripDetailPublisher: Published<TripDetail>.Publisher {
+        $tripDetail
+    }
+
+    init(tripId: UUID) {
+        self.tripId = tripId
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.tripDetail = TripDetail(
+                id: tripId,
+                title: .AppStrings.AppTitles.tripDetailTitle,
+                price: .AppIntegers.tripPricePlaceholder,
+                startsAt: .now,
+                finishAt: .now,
+                admin: User(phoneNumber: "+79173981189"),
+                members: [
+                    User(phoneNumber: "+79173981190"),
+                    User(phoneNumber: "+79173981129"),
+                    User(phoneNumber: "+79183981129")
+                ]
+            )
+        }
+    }
+}
